27832e6c8769f9f00b20560e5ad8b91035d87d16
MinGook <mgk8609@naver.com>
2021-11-23 14:28:29 +0900

    login 뮤테이션 완료

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { client } from "./Apollo";
+import { client, isLoggedInVar } from "./Apollo";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
 
 function App() {
-  const isLoggedIn = false;
   return (
     <ApolloProvider client={client}>
       <div className="App">
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
+            <Route path="/" element={isLoggedInVar ? <Home /> : <Login />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,16 +1,47 @@
+import { useQuery, gql, useMutation } from "@apollo/client";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { logUserIn } from "../Apollo";
+
+const LOGIN_MUTATION = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+      error
+      ok
+    }
+  }
+`;
 
 export default function Login() {
+  const onCompleted = data => {
+    const { token, ok, error } = data;
+    if (ok) {
+      logUserIn(token);
+    } else {
+      return setError("result", {
+        message: error
+      });
+    }
+  };
+  const [login, { data, loading, error }] = useMutation(LOGIN_MUTATION, {
+    onCompleted
+  });
   const {
+    setError,
     register,
     handleSubmit,
     watch,
     formState: { errors, isValid }
   } = useForm();
+
   const onSubmit = data => {
     const { username, password } = data;
-    console.log(username, password);
+    if (data) {
+      login({
+        variables: { username, password }
+      });
+    }
   };
 
   return (
