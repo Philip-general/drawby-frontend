57d3f3af0ce6185a7126345ca761426defe261bb
kunholee_98 <dlrjsgh750@naver.com>
2021-11-23 17:21:18 +0900

    간단한 layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { client } from "./Apollo";
+import Layout from "./Layout";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
@@ -12,7 +13,18 @@ function App() {
       <div className="App">
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
+            <Route
+              path="/"
+              element={
+                isLoggedIn ? (
+                  <Layout>
+                    <Home />
+                  </Layout>
+                ) : (
+                  <Login />
+                )
+              }
+            />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
