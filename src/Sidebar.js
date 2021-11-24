73eafc9a9bea67c01fb92680376fc3e331d3f03e
kunholee_98 <dlrjsgh750@naver.com>
2021-11-24 23:54:44 +0900

    해시태그, 콘테스트 더보기 팝업창

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import routes from "./routes";
@@ -7,6 +7,7 @@ const SidebarContainer = styled.div`
   width: 300px;
   background-color: #292929;
   margin-right: 10px;
+  max-height: 1000px;
 `;
 
 const SidebarBtn = styled.div`
@@ -21,10 +22,100 @@ const HashtagBtn = styled.div`
   margin-left: 50px;
   margin-top: 10px;
 `;
+const Modal = styled.div`
+  position: fixed;
+  top: 20%;
+  left: 25%;
+  width: 50%;
+  height: 60%;
+  justify-content: center;
+  align-items: center;
+  background-color: gray;
+`;
+
+const ModalHeader = styled.div`
+  justify-content: space-between;
+  margin-top: 10px;
+  display: flex;
+`;
+const ModalName = styled.div`
+  margin-left: 10px;
+`;
+
+const CloseBtn = styled.div`
+  background-color: red;
+  width: 50px;
+  height: 20px;
+`;
+
+const ModalBody = styled.div`
+  background-color: white;
+  width: 94%;
+  height: 60%;
+  margin: 3%;
+`;
+
+const ModalHashtagBtn = styled.button`
+  background-color: aliceblue;
+`;
+
+const ModalFooter = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 function Sidebar() {
+  // user가 follow하는 hashtag 중에 사이드바에 고정되어있는 것들
   const hashtags = ["bicycle", "tree", "movie"];
+  // contest 날짜 정보를 보여주는 것
   const contests = ["11월 첫째주", "11월 둘째주", "11월 셋째주"];
+  // user가 follow하는 모든 hashtag들
+  const modalHashtags = ["bicycle", "tree", "movie", "squid game", "Mac", "SF"];
+  const [hashtagMenu, setHashtagMenu] = useState(false);
+  const [contestMenu, setContestMenu] = useState(false);
+  const openHashtagMenu = () => {
+    setHashtagMenu(true);
+  };
+  const closeHashtagMenu = () => {
+    setHashtagMenu(false);
+  };
+  const openContestMenu = () => {
+    setContestMenu(true);
+  };
+  const closeContestMenu = () => {
+    setContestMenu(false);
+  };
+  function HashtagMenu() {
+    return (
+      <Modal>
+        <ModalHeader>
+          <ModalName>HashtagMenu</ModalName>
+          <CloseBtn onClick={closeHashtagMenu}>X</CloseBtn>
+        </ModalHeader>
+        <ModalBody>
+          {modalHashtags
+            ? modalHashtags.map(hashtag => (
+                <ModalHashtagBtn>{hashtag}</ModalHashtagBtn>
+              ))
+            : null}
+        </ModalBody>
+        <ModalFooter>
+          <CloseBtn onClick={closeHashtagMenu}>Save!</CloseBtn>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+  function ContestMenu() {
+    return (
+      <Modal>
+        <ModalHeader>
+          <ModalName>ContestMenu</ModalName>
+          <CloseBtn onClick={closeContestMenu}>X</CloseBtn>
+        </ModalHeader>
+        <ModalBody>여기는 달력이 들어갈 자리입니다.</ModalBody>
+      </Modal>
+    );
+  }
   return (
     <SidebarContainer>
       <Link to={routes.home}>
@@ -33,15 +124,15 @@ function Sidebar() {
       <Link to={routes.notYet}>
         <SidebarBtn>Go to LikeFeed</SidebarBtn>
       </Link>
-      {hashtags ? (
-        hashtags.map(hashtag => (
-          <Link to={routes.notYet}>
-            <HashtagBtn>{hashtag}</HashtagBtn>
-          </Link>
-        ))
-      ) : (
-        <HashtagBtn>follow 하러 가기</HashtagBtn>
-      )}
+      {hashtags
+        ? hashtags.map(hashtag => (
+            <Link to={routes.notYet}>
+              <HashtagBtn>{hashtag}</HashtagBtn>
+            </Link>
+          ))
+        : null}
+      <HashtagBtn onClick={openHashtagMenu}>follow 하러 가기</HashtagBtn>
+      {hashtagMenu ? <HashtagMenu /> : null}
       <Link to={routes.notYet}>
         <SidebarBtn>Go to ContestFeed</SidebarBtn>
       </Link>
@@ -50,6 +141,8 @@ function Sidebar() {
           <HashtagBtn>{contest}</HashtagBtn>
         </Link>
       ))}
+      <HashtagBtn onClick={openContestMenu}>더보기</HashtagBtn>
+      {contestMenu ? <ContestMenu /> : null}
     </SidebarContainer>
   );
 }
