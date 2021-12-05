1c2de2e172d91170e6af861707c905ab5a20e272
kunholee_98 <dlrjsgh750@naver.com>
2021-12-05 16:51:21 +0900

    FontSpan 컴포넌트

---
+++ src/components/Common/Commons.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+// font-family를 적용시켜야하는 경우, FontSpan 컴포넌트를 사용하길 추천합니다.
+export const FontSpan = styled.div`
+  font-family: "Noto Sans KR", sans-serif;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  letter-spacing: normal;
+  text-align: left;
+`;
