173689285678ea518841dd9123d3780c6a52e83c
kunholee_98 <dlrjsgh750@naver.com>
2021-12-06 18:07:28 +0900

    NotoSansKR 폰트 적용

--- src/components/Common/Commons.js
+++ src/components/Common/Commons.js
@@ -1,9 +1,8 @@
 import styled from "styled-components";
-
+import "../../NotoSansKR.css";
 // font-family를 적용시켜야하는 경우, FontSpan 컴포넌트를 사용하길 추천합니다.
 export const FontSpan = styled.div`
-  font-family: "Noto Sans KR", sans-serif;
-  font-weight: 500;
+  font-family: "Noto Sans KR";
   font-stretch: normal;
   font-style: normal;
   letter-spacing: normal;
