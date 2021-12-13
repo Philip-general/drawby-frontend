4de91c2ad335338d6f18ccca9538d3c9b0c959ca
kunholee_98 <dlrjsgh750@naver.com>
2021-12-13 22:35:36 +0900

    콘테스트 모달 디자인

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -80,9 +80,9 @@ const Modal = styled.div`
   position: fixed;
   z-index: 2;
   top: 20%;
-  left: 36%;
+  left: 33%;
   width: 500px;
-  height: 60%;
+  height: 424px;
   padding: 12px 12px 12px 16px;
   justify-content: center;
   align-items: center;
@@ -92,24 +92,26 @@ const Modal = styled.div`
 
 const ModalHeader = styled.div`
   justify-content: space-between;
-  margin-top: 10px;
+  margin: 10px 0 30px;
   display: flex;
 `;
-const ModalName = styled.div`
-  margin-left: 10px;
-`;
+const ModalName = styled(FontSpan)``;
 
-const CloseBtn = styled.div`
-  background-color: red;
-  width: 50px;
-  height: 20px;
+const CloseBtn = styled.img`
+  cursor: pointer;
+  width: 26px;
+  height: 26px;
 `;
 
 const ModalBody = styled.div`
   background-color: white;
-  width: 94%;
-  height: 60%;
-  margin: 3%;
+`;
+
+const CalanderHeader = styled.div`
+  height: 44px;
+  background-color: #fafafa;
+  border-radius: 10px;
+  padding: 10px 8px;
 `;
 
 const ModalHashtagBtn = styled.button`
@@ -143,8 +145,6 @@ function Sidebar({ openModal }) {
   const contests = weekList.map(
     weekInfo => `${weekInfo.year}년 ${weekInfo.month}월 ${weekInfo.weekNo}주차`
   );
-  console.log(weekList);
-  console.log(contests);
   // user가 follow하는 모든 hashtag들
   const modalHashtags = [];
   const [hashtagMenu, setHashtagMenu] = useState(false);
@@ -171,7 +171,7 @@ function Sidebar({ openModal }) {
         <Modal>
           <ModalHeader>
             <ModalName>HashtagMenu</ModalName>
-            <CloseBtn onClick={closeHashtagMenu}>X</CloseBtn>
+            <CloseBtn src="/PictureSrc/Exit.png" onClick={closeHashtagMenu} />
           </ModalHeader>
           <ModalBody>
             {modalHashtags
@@ -181,7 +181,7 @@ function Sidebar({ openModal }) {
               : null}
           </ModalBody>
           <ModalFooter>
-            <CloseBtn onClick={closeHashtagMenu}>Save!</CloseBtn>
+            <CloseBtn src="/PictureSrc/Exit.png" onClick={closeHashtagMenu} />
           </ModalFooter>
         </Modal>
       </ModalBackground>
@@ -192,10 +192,12 @@ function Sidebar({ openModal }) {
       <ModalBackground black="true" onClick={closeContestMenu}>
         <Modal>
           <ModalHeader>
-            <ModalName>ContestMenu</ModalName>
-            <CloseBtn onClick={closeContestMenu}>X</CloseBtn>
+            <ModalName>콘테스트 선택</ModalName>
+            <CloseBtn src="/PictureSrc/Exit.png" onClick={closeContestMenu} />
           </ModalHeader>
-          <ModalBody>여기는 달력이 들어갈 자리입니다.</ModalBody>
+          <ModalBody>
+            <CalanderHeader />
+          </ModalBody>
         </Modal>
       </ModalBackground>
     );
