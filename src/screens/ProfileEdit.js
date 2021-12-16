78b0285eb54e9121d297d5bed1a5f6d8727e65c2
kunholee_98 <dlrjsgh750@naver.com>
2021-12-16 14:44:05 +0900

    아바타, 이름, 패스워드, 소개글 변경 가능

--- src/screens/ProfileEdit.js
+++ src/screens/ProfileEdit.js
@@ -1,12 +1,106 @@
-import React from "react";
+import { gql, useMutation } from "@apollo/client";
+import React, { Fragment, useState } from "react";
+import { useForm } from "react-hook-form";
+import styled from "styled-components";
+import { Input, Textarea } from "../auth/Input";
+import UserIcon from "../components/Common/Avatar";
 import useUser from "../hooks/useUser";
+
+const EditContainer = styled.div`
+  display: flex;
+`;
+
+const UserContainer = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 100%;
+`;
+
+const TestView = styled.img`
+  width: inherit;
+  height: inherit;
+  border-radius: inherit;
+`;
+
+const Column = styled.div`
+  margin-right: 30px;
+`;
+
+const EDIT_PROFILE_MUTATION = gql`
+  mutation editProfile(
+    $username: String
+    $password: String
+    $avatar: Upload
+    $bio: String
+    $phoneNumber: String
+  ) {
+    editProfile(
+      username: $username
+      password: $password
+      avatar: $avatar
+      bio: $bio
+      phoneNumber: $phoneNumber
+    ) {
+      ok
+      error
+    }
+  }
+`;
 
 function ProfileEdit() {
   const { data } = useUser();
-  if (data?.me) {
-    console.log(data.me);
-  }
-  return <div>{data?.me?.username}'s profile edit page</div>;
+  const [preview, setPreview] = useState();
+  const [uploadFile, setUploadFile] = useState();
+  const { register, handleSubmit, getValues, setValue } = useForm();
+  const [editProfileMutation, { data: editData }] = useMutation(
+    EDIT_PROFILE_MUTATION,
+    { onCompleted: () => console.log(editData) }
+  );
+  const onValid = () => {
+    const { username, password, checkPassword, bio } = getValues();
+    console.log("in onValid");
+    editProfileMutation({
+      variables: { username, password, avatar: uploadFile, bio }
+    });
+  };
+  const showPicture = () => {
+    const picture = document.getElementById("file").files[0];
+    console.log(picture);
+    if (picture) {
+      setPreview(URL.createObjectURL(picture));
+    } else {
+      setPreview(false);
+    }
+    setUploadFile(picture);
+  };
+  return (
+    <Fragment>
+      <EditContainer>
+        <Column>
+          {preview ? (
+            <UserIcon size="100px" src={preview} />
+          ) : (
+            <UserIcon size="100px" src={data?.me?.avatar} />
+          )}
+          <Input
+            type="file"
+            placeholder="그림"
+            id="file"
+            onChange={() => showPicture()}
+          />
+        </Column>
+        <Column>
+          <form onSubmit={handleSubmit(onValid)}>
+            <Input placeholder="username" {...register("username")} />
+            <Input placeholder="password" {...register("password")} />
+            <Input placeholder="checkPassword" {...register("checkPassword")} />
+            <Textarea placeholder="bio" {...register("bio")} />
+            <button onClick={handleSubmit(onValid)}>submit</button>
+          </form>
+        </Column>
+      </EditContainer>
+    </Fragment>
+  );
 }
 
 export default ProfileEdit;
